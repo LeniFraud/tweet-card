4526f7c26aca3a2bfcc438032d23510dac2dd802
Anastasiia Klyosova <112250768+LeniFraud@users.noreply.github.com>
2023-02-16 15:43:32 +0200

    fixed ContactForm component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,41 +2,18 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Label, Input, Button } from './ContactForm.styled';
 
-// const INITIAL_STATE = {
-//   name: '',
-//   number: '',
-// };
-
 export const ContactForm = ({ onSubmit }) => {
-  // const [contactInfo, setContactInfo] = useState({ ...INITIAL_STATE });
-  // const { name, number } = contactInfo;
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  // const handleChange = e => {
-  //   const { name, value } = e.currentTarget;
-  //   setContactInfo(prevContactInfo => ({ ...prevContactInfo, [name]: value }));
-  // };
-
   const handleChange = e => {
     const { name, value } = e.currentTarget;
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-
-      default:
-        return;
-    }
+    if (name === 'name') return setName(value);
+    if (name === 'number') return setNumber(value);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    // const isSuccess = onSubmit({ ...contactInfo });
     const isSuccess = onSubmit(name, number);
     if (isSuccess) reset();
   };
